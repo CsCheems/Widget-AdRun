d35e5677363126e083a84186925c0684d87da6aa
CsCheems <j.Dev.mx@outlook.com>
2025-05-22 14:33:06 -0600

    Se modifico el callback para vercel

--- api/auth.js
+++ api/auth.js
@@ -1,16 +1,16 @@
-// api/auth.js
-const CLIENT_ID = process.env.CLIENT_ID;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-
 export default function handler(req, res) {
-  const state = Math.random().toString(36).substring(2, 15);
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const REDIRECT_URI = process.env.REDIRECT_URI;
   const scope = "channel:read:ads channel:edit:commercial";
+  const state = Math.random().toString(36).substring(2, 15);
 
-  const url = `https://id.twitch.tv/oauth2/authorize?client_id=${CLIENT_ID}` +
+  const url = `https://id.twitch.tv/oauth2/authorize` +
+              `?client_id=${CLIENT_ID}` +
               `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
-              `&response_type=code&scope=${encodeURIComponent(scope)}` +
-              `&state=${state}&force_verify=true`;
+              `&response_type=code` +
+              `&scope=${encodeURIComponent(scope)}` +
+              `&state=${state}` +
+              `&force_verify=true`;
 
-  res.writeHead(302, { Location: url });
-  res.end();
+  res.redirect(url);
 }
